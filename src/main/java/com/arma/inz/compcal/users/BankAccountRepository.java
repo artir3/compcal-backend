323288a65e4e3c1ccf336ec80121677f3f67ca56
Artur Markowski <markowski.artur@outlook.com>
2018-12-04 00:20:38 +0100

    adding bank account repository

---
+++ src/main/java/com/arma/inz/compcal/users/BankAccountRepository.java
@@ -0,0 +1,6 @@
+package com.arma.inz.compcal.users;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BankAccountRepository extends JpaRepository<BankAccount, Long> {
+}
