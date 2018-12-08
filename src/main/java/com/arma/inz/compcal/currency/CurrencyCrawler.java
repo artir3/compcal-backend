c8160575ca50c6a159c13a1aa0ff608d8f0e94a1
Artur Markowski <markowski.artur@outlook.com>
2018-12-08 20:42:22 +0100

    currency crawler read lastInsertDate ad get values from this date

--- src/main/java/com/arma/inz/compcal/currency/CurrencyCrawler.java
+++ src/main/java/com/arma/inz/compcal/currency/CurrencyCrawler.java
@@ -1,6 +1,5 @@
 package com.arma.inz.compcal.currency;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -9,7 +8,9 @@ import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.Arrays;
 
 @Component
@@ -17,17 +18,21 @@ public class CurrencyCrawler {
 
     @Value("${crawler.nbp.api}")
     private String nbpApiTodayUrl;
-
-    @Autowired
     private CurrencyRepository currencyRepository;
+
+    public CurrencyCrawler(CurrencyRepository currencyRepository){
+        this.currencyRepository = currencyRepository;
+    }
 
     @Scheduled(cron = "${crawler.nbp.api.crone}")
     public void getNbpApiTableA(){
-        long count = currencyRepository.count();
-        if (count == 0){
-            parseLastFifty();
+        LocalDate lastInsertDate = currencyRepository.getLastInsertDate();
+        if (lastInsertDate == null){
+            parseLastX(50);
         } else {
-            parseToday();
+            long daysBetween = ChronoUnit.DAYS.between(lastInsertDate, LocalDate.now());
+            if (daysBetween > 0)
+                parseLastX(daysBetween);
         }
     }
 
@@ -36,8 +41,8 @@ public class CurrencyCrawler {
         parseAndSaveTableA(response);
     }
 
-    private void parseLastFifty(){
-        CurrencyDayTableADTO[] response = getObjects(nbpApiTodayUrl + "/a/last/10");
+    private void parseLastX(long daysNo){
+        CurrencyDayTableADTO[] response = getObjects(nbpApiTodayUrl + "/a/last/" + daysNo);
         parseAndSaveTableA(response);
     }
 
@@ -46,7 +51,7 @@ public class CurrencyCrawler {
         HttpHeaders headers = new HttpHeaders();
         headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
         headers.setContentType(MediaType.APPLICATION_JSON);
-        HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+        HttpEntity<String> entity = new HttpEntity<>("parameters", headers);
         ResponseEntity<CurrencyDayTableADTO[]> respEntity = restTemplate.exchange(url, HttpMethod.GET, entity, CurrencyDayTableADTO[].class);
         return respEntity.getBody();
     }
--- src/main/java/com/arma/inz/compcal/currency/CurrencyRepository.java
+++ src/main/java/com/arma/inz/compcal/currency/CurrencyRepository.java
@@ -1,6 +1,12 @@
 package com.arma.inz.compcal.currency;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public interface CurrencyRepository extends JpaRepository<Currency, Long> {
+    @Query(value = "select distinct(c.added_at) from currency c order by c.added_at desc limit 1", nativeQuery = true)
+    LocalDate getLastInsertDate();
 }
