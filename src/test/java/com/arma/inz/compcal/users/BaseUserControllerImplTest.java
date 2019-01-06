965e304584747b8773d2c51f57f3d379288acc7b
Artur Markowski <markowski.artur@outlook.com>
2019-01-06 03:25:09 +0100

    dodanie testow

---
+++ src/test/java/com/arma/inz/compcal/users/BaseUserControllerImplTest.java
@@ -0,0 +1,66 @@
+package com.arma.inz.compcal.users;
+
+import com.arma.inz.compcal.bankaccount.BankAccountController;
+import com.arma.inz.compcal.database.DatabaseModelsInitialization;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class BaseUserControllerImplTest {
+
+    @Autowired
+    private BaseUserRepository repository;
+
+    @Autowired
+    private BankAccountController bankAccountController;
+
+    @Autowired
+    private BaseUserMailSender baseUserMailSender;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Before
+    public void setUp() throws Exception {
+        this.repository.save(DatabaseModelsInitialization.baseUser());
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        this.repository.deleteAll();
+    }
+
+    @Test
+    public void registration() {
+    }
+
+    @Test
+    public void login() {
+    }
+
+    @Test
+    public void loginByHash() {
+    }
+
+    @Test
+    public void getUserDTO() {
+    }
+
+    @Test
+    public void getBaseUser() {
+    }
+
+    @Test
+    public void updateBaseUser() {
+    }
+
+    @Test
+    public void authorize() {
+    }
+
+    @Test
+    public void registrationDate() {
+    }
+}
---
+++ src/test/java/com/arma/inz/compcal/users/BaseUserRepositoryTest.java
@@ -0,0 +1,57 @@
+package com.arma.inz.compcal.users;
+
+import com.arma.inz.compcal.database.DatabaseModelsInitialization;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@ActiveProfiles("test")
+public class BaseUserRepositoryTest {
+
+    @Autowired
+    private BaseUserRepository repository;
+
+    @Before
+    public void initialize() {
+        this.repository.save(DatabaseModelsInitialization.baseUser());
+    }
+
+    @After
+    public void clean() {
+        this.repository.deleteAll();
+    }
+
+    @Test
+    public void testFindByEmail() {
+        BaseUser user = this.repository.findByEmail("butu@mailinator.net");
+        assertThat(user.getHash()).isEqualTo("YnV0dUBtYWlsaW5hdG9yLm5ldDpjeXd5Z2V4YQ==");
+    }
+
+    @Test
+    public void testFindOneByHash() {
+        BaseUser user = this.repository.findOneByHash("YnV0dUBtYWlsaW5hdG9yLm5ldDpjeXd5Z2V4YQ==");
+        assertThat(user.getEmail()).isEqualTo("butu@mailinator.net");
+    }
+
+    @Test
+    public void testFindOneLikeHash() {
+        BaseUser user = this.repository.findOneLikeHash("YnV0dUBtYWlsaW5hdG9yLm5ldDpjeXd5Z2V4YQ");
+        assertThat(user.getEmail()).isEqualTo("butu@mailinator.net");
+    }
+
+    @Test
+    public void testNotFindOneLikeHash() {
+        BaseUser user = this.repository.findOneLikeHash("YnV0dUBtYWlsaadsW5hddG9yLm5ldDpjeXd5Z2V4YQ");
+        assertThat(user).isNull();
+    }
+
+}
